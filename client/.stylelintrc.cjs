38e280b60053768d5695e29b4f1c9e41b186035a
defective-origin <defective.origin@gmail.com>
2023-07-05 07:59:17 +0300

    feat: setup prettier

--- client/.stylelintrc.cjs
+++ client/.stylelintrc.cjs
@@ -1,7 +1,16 @@
 module.exports = {
-  'extends': [
+  extends: [
     'stylelint-config-standard-scss',
     'stylelint-prettier/recommended',
   ],
-  'plugins': ['stylelint-scss'],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'color-hex-length': 'long',
+    'color-hex-case': 'upper',
+    'color-no-hex': false,
+    'color-no-invalid-hex': true,
+    'string-quotes': 'single',
+    'no-extra-semicolons': true,
+    'block-no-empty': false,
+  },
 }
