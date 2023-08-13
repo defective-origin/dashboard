b45126c778de0816f3db021534271bebea2d560d
defective-origin <defective.origin@gmail.com>
2023-08-13 04:16:21 +0300

    feat: add test board

--- client/src/pages/DashboardPage/DashboardPage.component.tsx
+++ client/src/pages/DashboardPage/DashboardPage.component.tsx
@@ -37,12 +37,17 @@ const cards: BoardItem[] = [
 export function DashboardPage(props: DashboardPageProps): JSX.Element {
   const { navigate, children, className, ...otherProps } = props
   const _className = cn(css.DashboardPage, className)
-  const [items, setItems] = useState(cards)
-  const handleError = (error: any) => { console.log('handleError', error) }
-  const handleSelect = (placement: any) => { setItems([...items, { placement }]) }
-  const handleReselect = (item: BoardItem, oldItem: BoardItem) => {
-    setItems(items.map((i) => (i === oldItem ? item : i)))
+  const [items1, setItems1] = useState([...cards])
+  const [items2, setItems2] = useState([...cards])
+  const handleSelect1 = (placement: any) => { setItems1([...items1, { placement }]) }
+  const handleReselect1 = (item: BoardItem, oldItem: BoardItem) => {
+    setItems1(items1.map((i) => (i === oldItem ? item : i)))
   }
+  const handleSelect2 = (placement: any) => { setItems2([...items2, { placement }]) }
+  const handleReselect2 = (item: BoardItem, oldItem: BoardItem) => {
+    setItems2(items2.map((i) => (i === oldItem ? item : i)))
+  }
+  const handleError = (error: any) => { console.log('handleError', error) }
   const TestComponent = (props = {}) => <div {...props}>TEST ITEM</div>
 
   return (
@@ -53,12 +58,28 @@ export function DashboardPage(props: DashboardPageProps): JSX.Element {
         columns={9}
         gap={10}
         widget={TestComponent}
-        reselect={items[0]}
-        items={items}
+        reselect={items1[0]}
+        items={items1}
         select
-        onSelect={handleSelect}
-        onReselect={handleReselect}
+        onSelect={handleSelect1}
+        onReselect={handleReselect1}
         onError={handleError}
+        style={{ height: '50%', marginBottom: 10 }}
+      >
+        {children}
+      </Board>
+      <Board
+        className={_className}
+        rows={9}
+        columns={9}
+        gap={10}
+        widget={TestComponent}
+        items={items2}
+        select
+        onSelect={handleSelect2}
+        onReselect={handleReselect2}
+        onError={handleError}
+        style={{ height: '50%' }}
       >
         {children}
       </Board>
