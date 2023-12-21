cb24dd0fab2ede85db70abb174266b955073a0fc
Kseniya Boldak <117307630+KseniyaBoldak@users.noreply.github.com>
2023-12-21 08:00:53 +0300

    feat/ add menu, menbutton components (#7)
    * feat: add Menu components
    
    * fix: fix file name
    
    * fix: bugs
    
    ---------
    
    Co-authored-by: defective-origin <defective.origin@gmail.com>

---
+++ client/src/components/lib/Menu/Menu.component.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  MenuItemProps,
+  Menu as MuiMenu,
+  MenuList as MuiMenuList,
+  MenuProps as MuiMenuProps,
+} from '@mui/material'
+
+// ---| core |---
+// ---| pages |---
+// ---| screens |---
+// ---| components |---
+import Repeat from 'components/Repeat'
+
+// ---| common |---
+import { cn } from 'common/tools'
+
+// ---| self |---
+import css from './Menu.module.scss'
+import MenuItem from './MenuItem'
+
+export type MenuProps = MuiMenuProps & {
+  className?: string
+  children?: React.ReactNode
+  anchorEl?: HTMLElement | null
+  popup?: boolean
+  items?: MenuItemProps[]
+  onClose?: () => void
+}
+
+/**
+ * Component description.
+ *
+ * How to use
+ * @example
+ * <Menu />
+ */
+export function Menu(props: MenuProps): JSX.Element {
+  const { popup, items, children, className, ...otherProps } = props
+  const _className = cn(css.Menu, className)
+  const Tag = popup ? MuiMenu : MuiMenuList
+
+  return (
+    <Tag className={_className} {...(otherProps as any)}>
+      <Repeat as={Menu.Item} items={items} />
+      {children}
+    </Tag>
+  )
+}
+
+Menu.displayName = 'Menu'
+
+Menu.Item = MenuItem
+
+export default Menu
---
+++ client/src/components/lib/Menu/MenuItem/MenuItem.component.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import MuiMenuItem, {
+  MenuItemProps as MuiMenuItemProps,
+} from '@mui/material/MenuItem'
+
+// ---| core |---
+// ---| pages |---
+// ---| screens |---
+// ---| components |---
+import Link, { LinkProps } from 'components/lib/Link'
+
+// ---| common |---
+import { cn } from 'common/tools'
+
+// ---| self |---
+import css from './MenuItem.module.scss'
+
+export type MenuItemProps = MuiMenuItemProps &
+  Pick<LinkProps, 'start' | 'content' | 'end' | 'size' | 'iconSize' | 'align'>
+
+/**
+ * Component description.
+ *
+ * How to use
+ * @example
+ * <Item />
+ */
+export function MenuItem(props: MenuItemProps): JSX.Element {
+  const {
+    align,
+    start,
+    end,
+    size,
+    iconSize,
+    content,
+    className,
+    ...otherProps
+  } = props
+  const _className = cn(css.MenuItem, className)
+
+  return (
+    <MuiMenuItem className={_className} {...otherProps}>
+      <Link
+        width='inherit'
+        start={start}
+        content={content}
+        end={end}
+        size={size}
+        iconSize={iconSize}
+        align={align}
+      />
+    </MuiMenuItem>
+  )
+}
+
+MenuItem.displayName = 'MenuItem'
+
+export default MenuItem
