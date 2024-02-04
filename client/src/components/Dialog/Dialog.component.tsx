6aa87028a9698a0d3f1a621416684a9bbcf9b530
Kseniya Boldak <kseniya.boldak@gmail.com>
2024-02-04 20:05:35 +0300

    [feat][000009]: Add Dialog, Modal and Drawer

---
+++ client/src/components/Dialog/Dialog.component.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useState } from 'react'
+import { Dialog as MuiDialog } from '@mui/material'
+
+// ---| core |---
+import { cn } from 'tools'
+
+// ---| pages |---
+// ---| screens |---
+// ---| components |---
+import Card from 'components/Card'
+
+// ---| self |---
+import css from './Dialog.module.scss'
+
+export type DialogProps = {
+  className?: string
+  children?: React.ReactNode
+}
+
+/**
+ * Component description.
+ *
+ * How to use
+ * @example
+ * <Dialog />
+ */
+export function Dialog(props: DialogProps): JSX.Element {
+  const { children, className, ...otherProps } = props
+  const _className = cn(css.Dialog, className)
+  const [open, isOpen] = useState(false)
+  const toggle = useCallback(() => isOpen((open) => !open), [])
+
+  // TODO: [kseniya_boldak] create DialogButton
+  return (
+    <MuiDialog
+      className={_className}
+      open={open}
+      onClose={toggle}
+      {...otherProps}
+    >
+      {children}
+    </MuiDialog>
+  )
+}
+
+Dialog.displayName = 'Dialog'
+
+Dialog.Title = Card.Header
+Dialog.Content = Card.Content
+Dialog.Actions = Card.Actions
+
+export default Dialog
---
+++ client/src/components/Drawer/index.ts
@@ -0,0 +1,7 @@
+/* INJECT_IMPORT_PLACE */
+
+/* INJECT_DEFAULT_EXPORT_PLACE */
+export { default } from './Drawer.component'
+
+/* INJECT_EXPORT_PLACE */
+export * from './Drawer.component'
