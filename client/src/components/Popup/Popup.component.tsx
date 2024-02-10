525e36fdf063d385ee63b52ba4e3cf04d946a606
Kseniya Boldak <kseniya.boldak@gmail.com>
2024-02-10 21:05:21 +0300

    [feat][0000010]: Add Popup

---
+++ client/src/components/Popup/Popup.component.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Popover, PopoverOrigin } from '@mui/material'
+
+// ---| core |---
+import { cn } from 'tools'
+
+// ---| pages |---
+// ---| screens |---
+// ---| components |---
+import { ButtonProps } from 'components/Button'
+import Card from 'components/Card'
+
+// ---| self |---
+import css from './Popup.module.scss'
+
+export type PopupVariant = 'left' | 'right' | 'top' | 'bottom' | 'center'
+
+export const POPUP_VARIANT_MAP: Record<PopupVariant, PopoverOrigin> = {
+  left: { vertical: 'top', horizontal: 'left' },
+  right: { vertical: 'top', horizontal: 'right' },
+  bottom: { vertical: 'bottom', horizontal: 'center' },
+  top: { vertical: 'top', horizontal: 'center' },
+  center: { vertical: 'center', horizontal: 'center' },
+}
+
+export type PopupProps = {
+  className?: string
+  children?: React.ReactNode
+  open: boolean
+  title?: React.ReactNode
+  content?: React.ReactNode
+  actions?: ButtonProps | ButtonProps[]
+  side: PopupVariant
+  onClose?: () => void
+}
+
+/**
+ * Component description.
+ *
+ * How to use
+ * @example
+ * <Popup />
+ */
+export function Popup(props: PopupProps): JSX.Element {
+  const { title, content, actions,side, onClose, children, className, ...otherProps } = props
+  const _className = cn(css.Popup, className)
+
+  return (
+    <Popover
+      className={_className}
+      onClose={onClose}
+      anchorOrigin={POPUP_VARIANT_MAP[side]}
+      {...otherProps}>
+      {/* fragment prevent error of click listener */}
+      <>
+        <Card
+          title={title}
+          content={content}
+          actions={actions}
+          onClose={onClose}
+        >
+          {children}
+        </Card>
+      </>
+    </Popover>
+  )
+}
+
+Popup.displayName = 'Popup'
+
+export default Popup
---
+++ client/src/components/Popup/Popup.test.tsx
@@ -0,0 +1,33 @@
+// ---| tests |---
+import { render } from 'tests'
+
+// ---| self |---
+import Popup from './Popup.component'
+// import usePopup from './Popup.hook'
+// import PopupProvider, Popup from './Popup.context'
+
+describe('[Popup] component', () => {
+  it('should render component', () => {
+    const container = render(<Popup />)
+
+    expect(container.snapshot()).toMatchSnapshot()
+  })
+})
+
+// describe('[Popup] hook', () => {
+//   it('should return options', () => {
+//     const { result } = renderHook(() => usePopup())
+// 
+//     expect(result.current).toBe(null)
+//   })
+// })
+
+// describe('[Popup] context', () => {
+//   const wrapper = (props: PopupProps) => <PopupProvider { ...props } />
+// 
+//   it('should return options', () => {
+//     const { result } = renderHook(() => usePopup(), { wrapper })
+// 
+//     expect(result.current).toBeTruthy()
+//   })
+// })
