3b105366b1ca27b7edd8a0db9d777247241c3bcd
defective-origin <defective.origin@gmail.com>
2024-03-01 07:03:05 +0300

    [refactor][0000000]: Update breakpoints hook

--- client/src/hooks/device/UseBreakpoints/UseBreakpoints.hook.ts
+++ client/src/hooks/device/UseBreakpoints/UseBreakpoints.hook.ts
@@ -1,53 +1,46 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
-export class Breakpoint {
+export class Breakpoint<T extends string> {
   constructor(
+    public name: T,
     public from: number,
     public to: number,
   ) {}
 }
 
+export type Breakpoints<T extends string = string> = Breakpoint<T>[];
+
 // related to breakpoint documentation https://zeroheight.com/3f9f837f9/p/71b709-layout/b/56984a
-export const BREAKPOINTS = {
+export const BREAKPOINTS = [
 // Tv or High resolution Desktops
-  tv: new Breakpoint(1536, Number.MAX_SAFE_INTEGER),
+  new Breakpoint('tv', 1536, Number.MAX_SAFE_INTEGER),
 
   // Low resolutions Desktop or High resolution Desktops with 150% system zoom
-  desktop: new Breakpoint(1280, 1536),
+  new Breakpoint('desktop', 1280, 1536),
 
   // Tablet or Low resolution Desktops
-  tablet: new Breakpoint(768, 1280),
-  'tablet-horizontal': new Breakpoint(1024, 1280),
-  'tablet-vertical': new Breakpoint(768, 1024),
+  new Breakpoint('tablet', 768, 1280),
+  new Breakpoint('tablet-horizontal', 1024, 1280),
+  new Breakpoint('tablet-vertical', 768, 1024),
 
   // Mobile platforms
-  mobile: new Breakpoint(0, 768),
-  'mobile-horizontal': new Breakpoint(480, 768),
-  'mobile-vertical': new Breakpoint(0, 480),
+  new Breakpoint('mobile', 0, 768),
+  new Breakpoint('mobile-horizontal', 480, 768),
+  new Breakpoint('mobile-vertical', 0, 480),
+]
+
+const initNames = <T extends string>(width: number, breakpoints: Breakpoints<T>) => {
+  return breakpoints
+    .filter((breakpoint) => width >= breakpoint.from && width < breakpoint.to)
+    .map((breakpoint) => breakpoint.name)
 }
 
-export type Breakpoints = typeof BREAKPOINTS;
-export type BreakpointName = keyof Breakpoints;
-export const BREAKPOINT_NAMES = Object.keys(BREAKPOINTS) as BreakpointName[]
+export type BreakpointsOptions<T extends string> = Breakpoints<T>
 
-export const isInSizeRange = (current: number, point: Breakpoint) => current >= point.from && current < point.to
-
-const initOptions = (width: number, breakpoints: Breakpoints) => ({
-  names: BREAKPOINT_NAMES.filter((name) => isInSizeRange(width, breakpoints[name])),
-
-  isTv: isInSizeRange(width, breakpoints.tv),
-  isDesktop: isInSizeRange(width, breakpoints.desktop),
-  isTablet: isInSizeRange(width, breakpoints.tablet),
-  isTabletHorizontal: isInSizeRange(width, breakpoints['tablet-horizontal']),
-  isTabletVertical: isInSizeRange(width, breakpoints['tablet-vertical']),
-  isMobile: isInSizeRange(width, breakpoints.mobile),
-  isMobileHorizontal: isInSizeRange(width, breakpoints['mobile-horizontal']),
-  isMobileVertical: isInSizeRange(width, breakpoints['mobile-vertical']),
-})
-
-export type BreakpointsOptions = Breakpoints
-
-export type BreakpointsReturnOptions = ReturnType<typeof initOptions>
+export type BreakpointsReturnOptions<T extends string> = {
+  names: T[]
+  isActive: (breakpoint: T) => boolean
+}
 
 /**
  * Detect media breakpoints.
@@ -55,11 +48,12 @@ export type BreakpointsReturnOptions = ReturnType<typeof initOptions>
  * @example
  * const options = useBreakpoints()
  */
-export const useBreakpoints = (options: BreakpointsOptions = BREAKPOINTS): BreakpointsReturnOptions => {
-  const [state, setState] = useState(initOptions(window.innerWidth, options))
+export const useBreakpoints = <T extends string = typeof BREAKPOINTS[number]['name']>(options = BREAKPOINTS as BreakpointsOptions<T>): BreakpointsReturnOptions<T> => {
+  const [names, setNames] = useState(() => initNames(window.innerWidth, options))
+  const isActive = useCallback((name: T) => names.includes(name), [names])
 
   useEffect(() => {
-    const handleResize = () => setState(initOptions(window.innerWidth, options))
+    const handleResize = () => setNames(initNames(window.innerWidth, options))
 
     window.addEventListener('resize', handleResize)
 
@@ -68,7 +62,7 @@ export const useBreakpoints = (options: BreakpointsOptions = BREAKPOINTS): Break
     }
   }, [options])
 
-  return state
+  return { names, isActive }
 }
 
 export default useBreakpoints
