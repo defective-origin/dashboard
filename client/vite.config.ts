770b2fcc24046e02bdc7b8312af517bbfc631d39
Kseniya Boldak <kseniya.boldak@gmail.com>
2024-04-21 23:27:21 +0300

    [feat][0000000]: Setup gh-page

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/dashboard/',
   test: {
     globals: true,
     environment: 'jsdom',
