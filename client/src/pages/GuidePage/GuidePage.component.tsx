9409de62c60272a01a868214262c1188077586eb
defective-origin <defective.origin@gmail.com>
2024-06-19 00:27:14 +0300

    [feat][000000]: add Guide page

--- client/src/pages/GuidePage/GuidePage.component.tsx
+++ client/src/pages/GuidePage/GuidePage.component.tsx
@@ -7,9 +7,12 @@ import { cn } from 'tools'
 import Page, { PageProps } from 'pages/Page'
 
 // ---| screens |---
-import PlaceholderCard from 'screens/cards/PlaceholderCard'
-
 // ---| components |---
+import Text from 'components/Text'
+import Image from 'components/Image'
+import Block from 'components/Block'
+import Actions from 'components/Actions'
+
 // ---| self |---
 import css from './GuidePage.module.scss'
 
@@ -25,16 +28,103 @@ export type GuidePageProps = PageProps
 export function GuidePage(props: GuidePageProps): JSX.Element {
   const { children, className, ...otherProps } = props
   const _className = cn(css.GuidePage, className)
+  const style = { justifyContent: 'left' }
 
   return (
     <Page className={_className} name='PAGES.GUIDE' {...otherProps}>
-      <Page.Content v='board' columns={8}>
-        <PlaceholderCard name='Page Menu' area='1 / 1 / 2 / 2' />
-        <PlaceholderCard name='Content' area='1 / 2 / 2 / 8' />
-        <PlaceholderCard name='Content Menu' area='1 / 8 / 2 / 9' />
+      <Page.LeftAside width={400} p='xs'>
+        <Text content='Content' />
+
+        <Actions
+          v='y'
+          size='xs'
+          aligns='stretch'
+          items={[
+            { content: 'Title 1', style },
+            { content: 'Title 2', style },
+            { content: 'Title 3', style },
+            { content: 'Title 4', style },
+            { content: 'Title 5', style },
+            { content: 'Title 6', style },
+            { content: 'Title 7', style },
+            { content: 'Title 8', style },
+          ]}
+        />
+      </Page.LeftAside>
+
+      <Page.Content p='xs' g='sm' scroll='y'>
+        <Text v='h2' content='Topic' />
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 1' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 2' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 3' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 4' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 5' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 6' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 7' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
+
+        <Block height='min-content'>
+          <Text v='h3' content='Title 8' />
+          <Text v='body2' content={'content '.repeat(50)} />
+          <Image width='100%' height={300} />
+        </Block>
 
         {children}
       </Page.Content>
+
+      <Page.RightAside width={400} p='xs'>
+        <Text content='Topics' />
+
+        <Actions
+          v='y'
+          size='xs'
+          aligns='stretch'
+          items={[
+            { content: 'Topic 1', style },
+            { content: 'Topic 2', style },
+            { content: 'Topic 3', style },
+            { content: 'Topic 4', style },
+            { content: 'Topic 5', style },
+            { content: 'Topic 6', style },
+            { content: 'Topic 7', style },
+            { content: 'Topic 8', style },
+          ]}
+        />
+      </Page.RightAside>
     </Page>
   )
 }
